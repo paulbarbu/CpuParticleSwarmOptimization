ea278f69f631c905924ef54594f60fa005cc27c8
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2016-12-01 11:28:03 +0200

    added instructions on the Java Main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,13 @@
 package main;
 
 /**
+ * Add the Scala Library (org.scala-lang.scala-library_2.11.8.v20160304-115712-1706a37eb8)
+ * to Build Path as External JAR (see the eclipse/plugins) directory for the scala-library.jar file
+ * File - Export
+ * Choose this file as Main
  * 
+ * Then in the CLI:
  * java -jar cpuPso.jar
- *
  */
 public class Main { 
     public static void main (String[] args) {    	
